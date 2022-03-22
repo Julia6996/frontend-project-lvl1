1097672b91bc21ecd844739de00f4b7b387d1c04
Julia6996 <ronin1991@rambler.ru>
2022-03-22 15:06:21 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export const getRandomInt = (min, max) => {
+  // eslint-disable-next-line no-param-reassign
   min = Math.ceil(min);
+  // eslint-disable-next-line no-param-reassign
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
