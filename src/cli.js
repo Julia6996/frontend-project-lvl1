cf51cae9e7b4f8c72d295eb683805c15d670a0a6
Julia6996 <ronin1991@rambler.ru>
2022-03-30 10:07:53 +0300

    src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -6,11 +6,3 @@ export default function sayHi() {
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
 }
-
-export const getNameAndSayHi = (description) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`);
-  console.log(description);
-  return userName;
-};
