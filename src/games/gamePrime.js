f7abbd02242cb713f1b60520bfd69f298b682423
Julia6996 <ronin1991@rambler.ru>
2022-04-07 08:14:43 +0300

    add gamePrime.js

--- src/games/gamePrime.js
+++ src/games/gamePrime.js
@@ -5,19 +5,19 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (num) => {
   if (num < 2) {
-    return 'no';
+    return false;
   }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const createRound = () => {
   const randomNum = getRandomInt(1, 50);
-  const trueAnswer = isPrime(randomNum);
+  const trueAnswer = isPrime(randomNum) ? 'yes' : 'no';
   const question = `Question: ${randomNum}`;
   return [trueAnswer, question];
 };
